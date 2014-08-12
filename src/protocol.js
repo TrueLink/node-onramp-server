cedd34bd3176a81a68e4553d83018289ca05a275
Evgenus <chernyshov.eugene@gmail.com>
2014-08-13 01:42:25 +0300

    protocol

---
+++ src/protocol.js
@@ -0,0 +1,81 @@
+﻿var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+exports.name = "rampage";
+
+var MESSAGE_TYPE_PLAIN = 0;
+
+var MESSAGE_TYPE_RELAY = 10;
+var MESSAGE_TYPE_RELAYED = 11;
+
+var MESSAGE_TYPE_RTC_OFFER = 20;
+var MESSAGE_TYPE_RTC_ANSWER = 21;
+var MESSAGE_TYPE_RTC_ICE_CANDIDATE = 22;
+
+function parse(plain) {
+    return null;
+}
+exports.parse = parse;
+
+function plain(text) {
+    return new PlainMessage(MESSAGE_TYPE_PLAIN, text);
+}
+exports.plain = plain;
+
+function plain(text) {
+    return new PlainMessage(MESSAGE_TYPE_PLAIN, text);
+}
+exports.plain = plain;
+
+// _________________________________________________________________________ //
+var Message = (function () {
+    function Message(type) {
+        this._type = type;
+    }
+    Object.defineProperty(Message.prototype, "type", {
+        get: function () {
+            return this._type;
+        },
+        enumerable: true,
+        configurable: true
+    });
+
+    Message.prototype.getData = function () {
+        var data = {};
+
+        data["type"] = this._type;
+
+        return data;
+    };
+    return Message;
+})();
+exports.Message = Message;
+
+var PlainMessage = (function (_super) {
+    __extends(PlainMessage, _super);
+    function PlainMessage(type, text) {
+        _super.call(this, type);
+        this._text = text;
+    }
+    Object.defineProperty(PlainMessage.prototype, "text", {
+        get: function () {
+            return this._text;
+        },
+        enumerable: true,
+        configurable: true
+    });
+
+    PlainMessage.prototype.getData = function () {
+        var data = _super.prototype.getData.call(this);
+
+        data["text"] = this._text;
+
+        return data;
+    };
+    return PlainMessage;
+})(Message);
+exports.PlainMessage = PlainMessage;
+//# sourceMappingURL=protocol.js.map
---
+++ src/protocol.ts
@@ -0,0 +1,65 @@
+﻿export var name: string = "rampage";
+
+var MESSAGE_TYPE_PLAIN = 0;
+
+var MESSAGE_TYPE_RELAY = 10;
+var MESSAGE_TYPE_RELAYED = 11;
+
+var MESSAGE_TYPE_RTC_OFFER = 20;
+var MESSAGE_TYPE_RTC_ANSWER = 21;
+var MESSAGE_TYPE_RTC_ICE_CANDIDATE = 22;
+
+export function parse(plain: Object): Message {
+    return null;
+}
+
+export function plain(text: string): PlainMessage {
+    return new PlainMessage(MESSAGE_TYPE_PLAIN, text);
+}
+
+export function plain(text: string): PlainMessage {
+    return new PlainMessage(MESSAGE_TYPE_PLAIN, text);
+}
+
+// _________________________________________________________________________ //
+
+export class Message {
+    public get type(): number {
+        return this._type;
+    }
+
+    private _type: number;
+
+    constructor(type: number) {
+        this._type = type;
+    }
+
+    public getData(): Object {
+        var data = {};
+
+        data["type"] = this._type;
+
+        return data;
+    }
+}
+
+export class PlainMessage extends Message {
+    public get text(): string {
+        return this._text;
+    }
+
+    private _text: string;
+
+    constructor(type: number, text: string) {
+        super(type)
+        this._text = text;
+    }
+
+    public getData(): Object {
+        var data = super.getData();
+
+        data["text"] = this._text;
+
+        return data;
+    }
+}

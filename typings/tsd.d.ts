4d6e1d5c2fbb59af9d1a36b7f5529883f08a9fe2
Evgenus <chernyshov.eugene@gmail.com>
2014-08-23 03:28:42 +0300

    properly installed typings

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="node/node.d.ts" />
 /// <reference path="node-uuid/node-uuid.d.ts" />
 /// <reference path="websocket/websocket.d.ts" />
+/// <reference path="commander/commander.d.ts" />

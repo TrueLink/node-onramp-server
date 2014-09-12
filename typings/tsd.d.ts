192e2b3935e4fc97dd6daf49713959ecafd4b39f
Evgenus <chernyshov.eugene@gmail.com>
2014-09-12 17:44:38 +0300

    proper place for dependency

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -2,3 +2,4 @@
 /// <reference path="node-uuid/node-uuid.d.ts" />
 /// <reference path="websocket/websocket.d.ts" />
 /// <reference path="commander/commander.d.ts" />
+/// <reference path="../node_modules/browser-relay-client/index.d.ts" />
